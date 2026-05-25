65e532cd7168dfd909adf054faee813f6a2bf2e1
Vitor Siqueira <vitorccsiqueira@gmail.com>
2026-05-24 22:22:41 -0300

    add CAMERA_MODEL_AI_THINKER_V2 model

--- lib/Config/src/Config.h
+++ lib/Config/src/Config.h
@@ -4,8 +4,9 @@
 
 // Access Point mode
 // + true = creates a WiFi network without internet connection 
-// the car IP will be always 192.168.4.1:8000
+// the car IP will always be http://192.168.4.1:8000
 // + false = connect to an existing WiFi with internet connection
+// the car IP will be assigned by the router http://X.X.X.X:8000
 #define WIFI_AP_MODE true
 
 // Set minimum motor speed (0 to 255)
@@ -22,6 +23,10 @@
 // Set JPEG quality (0 to 63 - lower means higher quality)
 // NOTE: Higher image quality reduces CPU usage for compression.
 #define JPEG_QUALITY 15
+
+// Improve FPS by using double buffering (usually works perfectly
+// for ESP32-S3 family)
+#define INCREASE_FPS true
 
 // Customize PINS
 #if defined(CAMERA_FREENOVE_ESP32S3_CAM)
@@ -41,6 +46,16 @@
 #define PIN_M2_IN2 4
 #define PIN_FRONT_LED 9
 #define PIN_BOARD_LED 7
+// NOTE: higher resolutions affect framerate
+#define FRAME_SIZE FRAMESIZE_VGA
+
+#elif defined(CAMERA_MODEL_AI_THINKER_V2)
+#define PIN_M1_IN1 14
+#define PIN_M1_IN2 47
+#define PIN_M2_IN1 38
+#define PIN_M2_IN2 39
+#define PIN_FRONT_LED 40
+#define PIN_RGB_LED 48
 // NOTE: higher resolutions affect framerate
 #define FRAME_SIZE FRAMESIZE_VGA
 
